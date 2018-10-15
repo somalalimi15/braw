70ec59749c4486631bad5137ff2dc75f9a34a334
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-10-15 18:08:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,10 @@
 const Discord = require("discord.js")
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    client.channels.find(c => c.id === '498284777326247936').join();
-});
-
     client.on('ready', () => {
-     client.user.setActivity("- BrawlhallaSK | Road To 4K .",{type: 'Playing'});
+     client.user.setActivity("Hii .",{type: 'Listening'});
 
 });
-
 
 
 client.login(process.env.BOT_TOKEN);
