a9ca4bef86de2366d04b52ad33bccb61dc81d638
somalalimi15 <43244771+somalalimi15@users.noreply.github.com>
2018-11-04 19:05:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,1 @@
-const Discord = require("discord.js")
-const client = new Discord.Client();
-
-    client.on('ready', () => {
-     client.user.setActivity("Hii .",{type: 'Listening'});
-
-});
-
-
 
